ecb14eefdfce621f4e83ac496b833d37a01569f2
jeffwbuchanan@gmail.com <jeffwbuchanan@gmail.com>
2018-03-22 19:44:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemName = item;
+  var itemPrice = Math.floor(Math.random() * (100-1)) + 1;
+  var newCart = Object.assign({}, { [item] : itemPrice });
+  cart.push(newCart);
+  console.log('${item} has been added to your cart.');
+  return cart;
 }
 
 function viewCart() {
